388ba1bacb3bdaadf6f2dddb3e5c664ef64c6d10
Jayanth Pasupulati <63755061+jayanthnagasai@users.noreply.github.com>
2020-06-19 13:07:11 -0400

    N/A

---
+++ Burrito Maker.py
@@ -0,0 +1,49 @@
+
+class Burrito:
+     def tortilla(self):
+        tortilla1 = input("Do you want Flour tortilla or Corn Tortilla:")
+        print(tortilla1)
+     def rice(self):
+        rice1 = input("Do you want White Rice or Brown Rice:")
+        print(rice1)
+     def beans(self):
+        beans1 = input("Do you want Black Beans or Pinto Beans:")
+        print(beans1)
+     def meat(self):
+        meat1 = input("Do you want Chicken and Steak:")
+        print(meat1)
+     def salsa(self):
+        salsa1 = input("Do you want Hot salsa or Mild salsa:")
+        print(salsa1)
+     def guacmole(self):
+        beans1 = input("Do you want Guacmole or not:")
+        print(beans1)
+     def veggies(self):
+        veggies1 = input("Do you want Veggies or not:")
+        print(veggies1)
+     def sour_cream(self):
+        sour_cream1 = input("Do you want Sour Cream or not:")
+        print(sour_cream1)
+     def cheese(self):
+        cheese1 = input("Do you want Cheese or not:")
+        print(cheese1)
+     def corn(self):
+        sour_cream1 = input("Do you want Corn or not:")
+        print(sour_cream1)
+     def confirmation(self):
+        print("Your order is confirmed. Your Yummy Burrito will be there in the next 5-10 min")
+
+myBurrito = Burrito()
+
+myBurrito.tortilla()
+myBurrito.rice()
+myBurrito.beans()
+myBurrito.meat()
+myBurrito.salsa()
+myBurrito.guacmole()
+myBurrito.veggies()
+myBurrito.sour_cream()
+myBurrito.cheese()
+myBurrito.corn()
+myBurrito.confirmation()
+
